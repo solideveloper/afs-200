c671deeb78fde832012835bc0bb788adc36fc4f1
developer-soli <soli.developed@gmail.com>
2021-08-21 16:49:26 -0400

    week 3 complete

---
+++ week3/split-join.py
@@ -0,0 +1,5 @@
+csv = "Eric John Michael Terry Graham Terry Brian"
+friend_list = csv.split()
+friend_list2 = "-".join(friend_list)
+print(friend_list)
+print(friend_list2)
---
+++ week3/tuple.py
@@ -0,0 +1,5 @@
+robert = ('Tomkins', '1988', 'Star Wars', 'Web Developer', 'NYC')
+print(robert[0])
+
+numbers = (1,2,3,4,5,6,7)
+print(robert, numbers[1:4])
